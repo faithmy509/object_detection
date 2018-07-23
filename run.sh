5549bb68b55aa3b820d0a55f3c2aeb828ce85cb8
penglong <184602748@qq.com>
2018-07-23 10:48:43 +0800

    third run

--- run.sh
+++ run.sh
@@ -49,7 +49,7 @@ pipeline_config_path=$output_dir/$config
 
 
 # 导出模型
-python ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-$current  --output_directory $output_dir/exported_graphs
+python ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-500  --output_directory $output_dir/exported_graphs
 
 # 在test.jpg上验证导出的模型
 python ./inference.py --output_dir=$output_dir --dataset_dir=$dataset_dir
